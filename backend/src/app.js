a1a0c40ba9fc783a88387ca65ee7f250822a8bec
mielindev <phamvietquang.dev@gmail.com>
2025-07-24 10:20:39 +0700

    ✨ feat(backend): initialize backend application with core modules and routes
    - add express as web framework
    - integrate mongoose for mongodb interaction
    - incorporate dotenv for environment variable management
    - use clerk for authentication middleware
    - implement routes for users, auth, admin, tracks, albums, and stats
    - create database connection
    - setup nodemon for development

---
+++ backend/src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { config } from "dotenv";
+import { clerkMiddleware } from "@clerk/express";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import adminRoutes from "./routes/admin.route.js";
+import trackRoutes from "./routes/track.route.js";
+import albumRoutes from "./routes/album.route.js";
+import statRoutes from "./routes/stat.route.js";
+import connectToDB from "./libs/db.js";
+
+config();
+const app = express();
+const PORT = process.env.PORT || 5005;
+
+app.use(clerkMiddleware());
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/tracks", trackRoutes);
+app.use("/api/albums", albumRoutes);
+app.use("/api/stats", statRoutes);
+
+app.listen(PORT, () => {
+  connectToDB();
+  console.log(`Server is running on port ${PORT}`);
+});
