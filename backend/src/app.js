bae07e460d2dd00e0eba6829111a4d2d0e9cbe8c
mielindev <phamvietquang.dev@gmail.com>
2025-07-24 16:23:50 +0700

    ✨ feat(app): add file upload functionality
    - install express-fileupload package
    - configure express-fileupload middleware with temp directory and file size limits
    - implement global error handling middleware for consistent error responses

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { config } from "dotenv";
+import path from "path";
 import { clerkMiddleware } from "@clerk/express";
+import fileUpload from "express-fileupload";
+
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import adminRoutes from "./routes/admin.route.js";
@@ -12,9 +15,20 @@ import connectToDB from "./libs/db.js";
 config();
 const app = express();
 const PORT = process.env.PORT || 5005;
+const __dirname = path.resolve();
 
 app.use(clerkMiddleware());
 app.use(express.json());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+      fileSize: 10 * 1024 * 1024, // 10MB max file size
+    },
+  })
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
@@ -22,6 +36,15 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/tracks", trackRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
+
+app.use((err, res) => {
+  return res.status(500).json({
+    message:
+      process.env.NODE_ENV === "production"
+        ? "Internal Server Error"
+        : err.message,
+  });
+});
 
 app.listen(PORT, () => {
   connectToDB();
