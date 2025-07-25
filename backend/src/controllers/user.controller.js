16113a5c0977637e825a283ade0e1997ab6cc878
mielindev <phamvietquang.dev@gmail.com>
2025-07-25 17:27:11 +0700

    ✨ feat(user): implement get all users endpoint
    - add new endpoint to fetch all users
    - protect the route with authentication middleware

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,1 +1,21 @@
-const userController = {};
+import User from "../models/user.model.js";
+
+const userController = {
+  getAllUsers: async (req, res, next) => {
+    try {
+      const currentUserId = req.auth.userId;
+      const users = await User.find({
+        clerkId: { $ne: currentUserId },
+      }).sort({ fullName: 1 });
+
+      return res.status(200).json(users);
+    } catch (error) {
+      console.log("Error in getAllUsers", error);
+      next(error);
+    }
+  },
+
+  //   TODO: get Messages
+};
+
+export default userController;
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,9 @@
 import express from "express";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+import userController from "../controllers/user.controller.js";
 
 const userRoutes = express.Router();
 
-userRoutes.get("/", (req, res) => {
-  res.send("get user route");
-});
+userRoutes.get("/", protectRoute, userController.getAllUsers);
 
 export default userRoutes;
