33fd00f7c5a451621d051710c7ddba99c2a1c2ad
mielindev <phamvietquang.dev@gmail.com>
2025-08-02 16:02:09 +0700

    ✨ feat(user): implement get messages functionality
    - add route to fetch messages between two users
    - implement controller logic to retrieve and return messages

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,9 +1,10 @@
+import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
 const userController = {
   getAllUsers: async (req, res, next) => {
     try {
-      const currentUserId = req.auth.userId;
+      const { userId: currentUserId } = req.auth();
       const users = await User.find({
         clerkId: { $ne: currentUserId },
       }).sort({ fullName: 1 });
@@ -15,7 +16,24 @@ const userController = {
     }
   },
 
-  //   TODO: get Messages
+  getMessages: async (req, res) => {
+    try {
+      const { userId: senderId } = req.auth();
+      const { userId: receiverId } = req.params;
+
+      const message = await Message.find({
+        $or: [
+          { senderId: senderId, receiverId: receiverId },
+          { senderId: receiverId, receiverId: senderId },
+        ],
+      }).sort({ createdAt: 1 });
+
+      return res.status(200).json(message);
+    } catch (error) {
+      console.log("Error in getMessages", error);
+      next(error);
+    }
+  },
 };
 
 export default userController;
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -5,5 +5,6 @@ import userController from "../controllers/user.controller.js";
 const userRoutes = express.Router();
 
 userRoutes.get("/", protectRoute, userController.getAllUsers);
+userRoutes.get("/message/:userId", protectRoute, userController.getMessages);
 
 export default userRoutes;
