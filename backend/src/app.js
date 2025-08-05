a532cefc1094738c73fc596d718006f4dbf2a320
mielindev <phamvietquang.dev@gmail.com>
2025-08-05 10:17:23 +0700

    ✨ feat(server): add production build support
    - serve static files from frontend/dist in production
    - create catch-all route to serve index.html for SPA

--- backend/src/app.js
+++ backend/src/app.js
@@ -53,6 +53,13 @@ app.use((err, res) => {
   });
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 server.listen(PORT, () => {
   connectToDB();
   console.log(`Server is running on port ${PORT}`);
